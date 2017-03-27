7f60617f42f55306507d5d5d3e486eadd7ab28d8
tbatie <tracy.batie@connexta.com>
2017-03-27 15:30:27 -0700

    Fixed schema.json artifact

--- ui/babel-relay-plugin.js
+++ ui/babel-relay-plugin.js
@@ -1,6 +1,6 @@
 const schema = require('./target/schema.json')
 
-module.exports = require('babel-relay-plugin')(schema, {
+module.exports = require('babel-relay-plugin')(schema.data, {
   debug: true,
   suppressWarnings: false,
   enforceSchema: true
